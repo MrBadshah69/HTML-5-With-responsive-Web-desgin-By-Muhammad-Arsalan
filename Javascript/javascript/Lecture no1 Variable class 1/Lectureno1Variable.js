9ab132bdd5d3d9171236a25986a7a5d234508cca
MrBadshah69 <mrbadshahbusiness@gmail.com>
2023-08-28 04:02:33 +0500

    Javascript Second Lecture com

---
+++ Javascript/javascript/Lecture no1 Variable class 1/Lectureno1Variable.js
@@ -0,0 +1,23 @@
+// document
+document.write("<h1>Javascript Fisrt Lecture</h1>");
+
+// // console
+console.log("Lectureno1Variable");
+
+// variable
+var Name = "Muhammmad Arsalan";
+document.write("<br><b>Name:</b>" + Name);
+
+// variable 02
+var FatherName = "Akbar Ali";
+document.write("<br><b>Father Name:</b>" + FatherName);
+
+// variable 03
+var CourseName = "Web Designing";
+document.write("<br><b>Course Name:</b>" + CourseName);
+
+// variable 04
+Timing ="5 TO 7 PM"
+document.write("<br><b>Timing:</b>"+Timing);
+
+console.log("End");
---
+++ Javascript/javascript/Lecture no2 Data type/Lectureno2Datatype.js
@@ -0,0 +1,41 @@
+// document
+document.write("<h1>Javascirpt 2nd Lecture</h1>");
+
+// console
+console.log("Lectureno1Datatype");
+
+// Data type
+
+// alert
+alert("You Are Hacked Brother😈");
+
+// confirm
+confirm("Again I am Tell you janaman You Are Hacked Brother😈");
+
+// Prompt
+
+// name
+Name = prompt("Enter You Name");
+document.write("<br><b>Name:</b>" + Name);
+
+// FatherName
+FatherName = prompt("Enter Your Father Name");
+document.write("<br><b>Father Name:</b>" + FatherName);
+
+// Contact number
+Contactnumber = prompt("Enter your contact number");
+document.write("<br><b>Contact Number:</b>" + Contactnumber);
+
+// Age
+age = prompt("Enter Your Age");
+document.write("<br><b>Age:</b>" + age);
+
+// CourseName
+CourseName = prompt("Enter Your Course Name");
+document.write("<br><b>Course Name:</b>" + CourseName);
+
+// Timing
+Timing = prompt("Enter Your Timing");
+document.write("<br><b>Timing:</b>" + Timing);
+
+console.log("END")
