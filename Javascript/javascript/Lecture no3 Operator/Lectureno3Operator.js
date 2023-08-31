20c256ed1e1b378a1091a38b787742e7712a3e40
MrBadshah69 <mrbadshahbusiness@gmail.com>
2023-09-01 02:21:21 +0500

    Javascript Third commit

---
+++ Javascript/javascript/Lecture no3 Operator/Lectureno3Operator.js
@@ -0,0 +1,22 @@
+console.log("Start")
+
+Answer = parseInt(prompt("Enter First number"))
+Answer = parseInt(prompt("Enter Second number"))
+
+
+Answer = num1 = "+" = num2 = "="
+Answer = num1 = "-" = num2 = "="
+Answer = num1 = "/" = num2 = "="
+Answer = num1 = "*" = num2 = "="
+Answer = num1 = "%" = num2 = "="
+
+
+
+document.write("Answer by Add" + num1 + "+" + num + "=")
+document.write("Answer by Sub" + num1 + "-" + num + "=")
+document.write("Answer by div" + num1 + "/" + num + "=")
+document.write("Answer by mul" + num1 + "*" + num + "=")
+document.write("Answer by rem" + num1 + "%" + num + "=")
+
+
+
